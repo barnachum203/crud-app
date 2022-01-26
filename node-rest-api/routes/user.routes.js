28ad0b6528392196edc8f4a9b9d989724f6a595a
barnachum203 <barnachum203@gmail.com>
2022-01-26 19:45:58 +0200

    use JoiMiddleware insted ErrorMiddleware

--- node-rest-api/routes/user.routes.js
+++ node-rest-api/routes/user.routes.js
@@ -2,14 +2,14 @@ var express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
-const errorMiddleware = require('../middleware/errorMiddleware');
-const { registerSchema, loginSchema } = require('../middleware/errorMiddleware');
+const joiMiddleware = require('../middleware/joiMiddleware');
+const { registerSchema, loginSchema } = require('../middleware/userValidators');
 const { requireUser } = require('../middleware/auth');
 
 router.get('/',requireUser, userController.getAll);
-router.post('/register', [errorMiddleware(registerSchema)],userController.register);
+router.post('/register', [joiMiddleware(registerSchema)],userController.register);
 router.put('/update/:id', requireUser, userController.update);
-router.post('/login',[errorMiddleware(loginSchema)], userController.login);
+router.post('/login',[joiMiddleware(loginSchema)], userController.login);
 router.delete('/delete/:id',requireUser, userController.deleteUser);
 router.get('/user/:id', requireUser,userController.getUserById);
 
